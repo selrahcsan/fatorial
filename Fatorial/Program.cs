32b3c4f63ed03663de4e4136d493c54790ead211
Charles Santana <csantanta@gmail.com>
2024-07-23 11:00:33 -0300

    Update Project

--- Fatorial/Program.cs
+++ Fatorial/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("Fatorial");
 
-Console.WriteLine("Hello, World!");
+Console.Write("Digite um número para acha o fatorial :");
+var numero = Convert.ToInt32(Console.ReadLine());
+var resultado = Fatorial(numero);
+Console.WriteLine($"O {numero}! é {resultado};");
+
+static long Fatorial(int n)
+{
+    if (n <= 1)
+        return 1;
+    else
+        return n * Fatorial(n - 1);
+}
+    
